84c468d7f4a4c9c3771bf45f93785135a2a255ac
Gabriel <gleffad.dev@gmail.com>
2020-10-20 20:34:03 +0200

    'erase'

--- gab/screens/Cart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text, View } from "react-native";
-
-export default class Cart extends React.Component {
-    render() {
-        return <View><Text>Cart</Text></View>;
-    }
-}
