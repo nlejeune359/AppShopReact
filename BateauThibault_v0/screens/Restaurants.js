c18cc59c9be5e58bd637bc64bc309d2789f975b4
Gabriel <gleffad.dev@gmail.com>
2020-10-22 17:36:01 +0200

    ajout resto

--- BateauThibault_v0/screens/Restaurants.js
+++ BateauThibault_v0/screens/Restaurants.js
@@ -1,27 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
-import Button from "../components/Button";
+import { ImageBackground, Text, View, StyleSheet } from "react-native";
 import { images } from "../constants/Images";
+import Button from "../components/Button";
 import Header from "../components/Header";
 
-export default class Restaurants extends React.Component {
+export default class RestaurantsList extends React.Component {
   state = {
-    data: require("../assets/data/restaurants.json")
-  }
+    data: require("../assets/data/restaurants.json"),
+  };
 
   render() {
+    let restaurants = this.props.route.params.data
+      ? this.props.route.params.data
+      : [];
+
     return (
       <View style={styles.container}>
         <ImageBackground source={images.background.uri} style={styles.image}>
           <Header navigation={this.props.navigation} />
-          <Text style={{ marginVertical: 10, textAlign: "center" }}>
-            Choisissez
-          </Text>
-          {
-            this.state.data.map() => {
-              return <Button text={x.name} key={index} screen="Home" navigation={this.props.navigation}></Button>
-            }
-          }
+
+          {restaurants != "sale"
+            ? this.state.data
+                .filter((el) => el.category == restaurants)
+                .map((x, index) => {
+                  return (
+                    <Button
+                      text={x.name}
+                      key={index}
+                      navigation={this.props.navigation}
+                      image={images.poulpe.uri}
+                    />
+                  );
+                })
+            : this.state.data
+                .filter((el) => el.sale)
+                .map((x, index) => {
+                  return (
+                    <Button
+                      text={x.name}
+                      key={index}
+                      navigation={this.props.navigation}
+                      image={images.poulpe.uri}
+                    />
+                  );
+                })}
         </ImageBackground>
       </View>
     );
@@ -33,6 +55,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
+
   image: {
     flex: 1,
     resizeMode: "cover",
